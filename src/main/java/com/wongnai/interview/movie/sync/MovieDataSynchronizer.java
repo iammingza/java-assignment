082500acb1db88e13d418ffac3acfd31e1a1756c
Nattapon <nattapon@Nattapon.local>
2019-05-17 01:07:26 +0700

    #3 Store data to DB and Can query data

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -1,12 +1,19 @@
 package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
+import javax.validation.constraints.Null;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +26,11 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		MoviesResponse data = movieDataService.fetchAll();
+		for(int i=0;i<data.size();i++){
+			Movie movie = new Movie(data.get(i).getTitle());
+			movie.setActors(data.get(i).getCast());
+			movieRepository.save(movie);
+		}
 	}
 }
